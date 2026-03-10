53add29c06d87a2d4ddd6a6e79831d5791634e4d
AravindSama <aravind.sama12@gmail.com>
2026-03-10 05:39:49 -0500

    scala create DF from RDD

--- src/main/scala/scala/practice/obj_SchemaRDD.scala
+++ src/main/scala/scala/practice/obj_SchemaRDD.scala
@@ -3,6 +3,7 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
 
 case class filedm1(
 		state:String,
@@ -27,9 +28,29 @@ object obj_SchemaRDD {
 						.filter(x => x.length >= 4) // keep only rows with at least 4 columns
 						.map(x => filedm1(x(0).trim, x(1).trim, x(2).trim, x(3).trim))
 						val dataframe_schema= inputColumns.toDF()
-						dataframe_schema.printSchema()
+						//dataframe_schema.printSchema()
 						val sel_col=dataframe_schema.select("state", "capital","language","country")
-						sel_col.show()
+						//sel_col.show()
+						val fil_data = sel_col.filter("language=='English'").show()
+
+						dataframe_schema.createOrReplaceTempView("country_table")
+						spark.sql("select state,capital,language,country from country_table").show()
+
+						val rowrdd=inputSplit.filter(x => x.length >= 4).map(x =>Row(x(0).toString().trim(),
+						    x(1).toString().trim(), 
+						    x(2).toString().trim(),
+						    x(3).toString().trim()))
+						val struct_schema=StructType(Array(
+								StructField("state",StringType,true),
+								StructField("capital",StringType,true),
+								StructField("language",StringType,true),
+								StructField("country",StringType,true)))
+
+						val struct_df=spark.createDataFrame(rowrdd, struct_schema)
+						struct_df.printSchema()
+						struct_df.show()
+						
+
 		}
 
 }
